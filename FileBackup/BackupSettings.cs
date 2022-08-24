e651e3f27303506f58928f7207cc54131b280c35
Igor Osipov <62898482+garinich777@users.noreply.github.com>
2022-08-24 14:19:28 +0300

    Добавьте файлы проекта.

---
+++ FileBackup/BackupSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace FileBackup
+{
+    public enum LogLevel
+    {
+        Error,
+        Info,
+        Debug
+    }
+
+    public class BackupSettings
+    {
+        public List<string> SourceFolders { get; set; }
+        public List<string> TargetFolders { get; set; }
+        public LogLevel LogLevel { get; set; }
+
+        public async void SaveSettings()
+        {
+            var JsonSerializerOptions = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string settings = JsonSerializer.Serialize(this, JsonSerializerOptions);
+            await File.WriteAllTextAsync("settings.json", settings, Encoding.Unicode);
+        }
+
+        public void LoadSettings()
+        {
+
+        }
+    }
+}
---
+++ FileBackup/Program.cs
@@ -0,0 +1,20 @@
+﻿using FileBackup;
+
+
+
+BackupSettings backupSettings = new BackupSettings()
+{
+    LogLevel = LogLevel.Info,
+    SourceFolders = new List<string>
+    {
+        @"C:\Users\laki\YandexDisk\Работа\Infotecs\Исходная папка"
+    },
+    TargetFolders = new List<string>
+    {
+        @"C:\Users\laki\YandexDisk\Работа\Infotecs\Целевая папка"
+    }
+};
+
+backupSettings.SaveSettings();
+
+Console.WriteLine("Hello, World!");
